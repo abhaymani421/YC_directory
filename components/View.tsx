e94558861c5119952d4ddba0f121d37075ee435b
Abhaymani Singh <abhaymanisingh421@gmail.com>
2025-07-30 00:00:11 +0530

    implemented incrementing view functionality in startup details page using server side rendering

--- components/View.tsx
+++ components/View.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import Ping from "./Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/query";
 import { client } from "@/sanity/lib/client";
-
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 const View = async ({ id }: { id: string }) => {
-  const { views: totalViews } = await client
+  const { views: totalViews } = await client // the api token helps recieved from sanity patches views or basically updates them
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
-  //TODO Update the numer of views when someone sees the post
+  after(async () => {
+    await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit();
+  });
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
@@ -22,3 +28,5 @@ const View = async ({ id }: { id: string }) => {
 };
 
 export default View;
+
+//VIEWS UPDATE USE SERVER SIDE RENDERING FOR INSTANT UPDATES
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 // if you make the useCdn as true, this will make sure to cache whatever content you request for 60sec
